a6a20936cda78370afdbacdd4858d9c853e2edd4
Cody <codylau6233@hotmail.com>
2023-11-20 23:57:46 -0800

    Refactor: remove test object from file

--- functions.js
+++ functions.js
@@ -12,18 +12,7 @@ const generateRandomString = () => {
   }
   return result;
 };
-const users = {
-  userRandomID: {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur",
-  },
-  user2RandomID: {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk",
-  },
-};
+
 /**
  * function look up user's email if user is in database
  * @param {object} object
