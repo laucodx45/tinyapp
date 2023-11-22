baa904741f920cfde8eb4360cf4eca78c07b5387
Cody <codylau6233@hotmail.com>
2023-11-21 19:21:53 -0800

    Refactor express_server.js: -refactor comments

--- express_server.js
+++ express_server.js
@@ -21,33 +21,13 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-const users = {
-  // userRandomID: {
-  //   id: "userRandomID",
-  //   email: "user@example.com",
-  //   password: "purple-monkey-dinosaur",
-  // },
-  // user2RandomID: {
-  //   id: "user2RandomID",
-  //   email: "user2@example.com",
-  //   password: "dishwasher-funk",
-  // },
-};
+const users = {};
 
 // ----------------------------------------------------------
 // ----------------------------------------------------------
 // Route Handlers
-
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
-
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 // route handler for /urls
@@ -69,26 +49,21 @@ app.get("/urls", (req, res) => {
   res.render('urls_index', templateVars);
 });
 
-// route handler for /urls/new, it renders the create new tinyURL submission form
 app.get("/urls/new", (req, res) => {
-  
   const templateVars = {user: users[req.cookies["user_id"]]};
   res.render("urls_new", templateVars);
 });
 
-// route handler for /register
 app.get("/register", (req, res) => {
   const templateVars = {user: users[req.cookies["user_id"]]};
   res.render("register", templateVars);
 });
 
-// route handler for get request to /login endpoint
 app.get("/login", (req, res) => {
   const templateVars = {user: users[req.cookies["user_id"]]};
   res.render("login", templateVars);
 });
 
-// handling POST requests to the "/urls" endpoint
 app.post("/urls", (req, res) => {
   const shortenedURL = generateRandomString();
 
@@ -98,20 +73,19 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortenedURL}`);
 });
 
-// handling POST request for shortURL deletion
+// route handler for POST request to delete shortURL
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect("/urls");
 });
 
-// when server receieve a post request /urls/:id, go into urlDatabase to change the longURL
+// post request to /urls/:id, request to change the longURL in urlDatabase
 app.post("/urls/:id", (req, res) => {
   // update the shortURL value from old longURL to the new longURL user submitted through edit
   urlDatabase[req.params.id] = req.body.longURL;
   res.redirect("/urls");
 });
 
-// post request to /login
 app.post("/login", (req, res) => {
   // req.body = {email: exampleEmail, password: examplePw}
   const userEmail = req.body.email;
@@ -119,14 +93,14 @@ app.post("/login", (req, res) => {
 
   // if email or password is empty send back a statusCode 400
   if (userEmail.length === 0 || userPassword.length === 0) {
-    res.status(400).send('Bad request, no email or password entered');
+    res.status(400).send('Bad request, both email and password are required to login');
     return;
   }
   // if email entered in login is in users database
   if (getUserByEmail(users, userEmail)) {
     const id = getUserByEmail(users, userEmail);
 
-    // check whether pw in users obj match with the one user used to login
+    // check whether password in users obj match with the one user entered to login
     if (users[id].password === userPassword) {
       res.cookie("user_id", id);
       res.redirect("/urls");
@@ -134,7 +108,7 @@ app.post("/login", (req, res) => {
     }
   }
   // if email is not in database or password does not match the one in database
-  res.status(403).send('email not in database, or password does not match');
+  res.status(403).send('Forbidden: Email not found in the database, or incorrect password.');
 });
 
 app.post("/logout", (req, res) => {
@@ -142,7 +116,7 @@ app.post("/logout", (req, res) => {
   res.redirect("/login");
 });
 
-// route handler for post request to /register
+// post request to /register endpoint
 app.post("/register", (req, res) => {
   const randomUserId = generateRandomString();
   
@@ -151,14 +125,14 @@ app.post("/register", (req, res) => {
   
   // if email or password is empty send back a statusCode 400
   if (userEmail.length === 0 || userPassword.length === 0) {
-    res.status(400).send('Bad request, no email or password entered');
+    res.status(400).send('Bad Request: Please provide both email and password.');
     return;
   }
 
   // check users object whether the email has been resgistered, if truthy
   if (getUserByEmail(users, userEmail)) {
     // if function returns true, email has already been registered
-    res.status(400).send(`This email has already been registered, try registering with another email`);
+    res.status(400).send(`This email has already been registered. Please use a different email`);
     return;
   }
 
@@ -186,7 +160,7 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-// route handler for /u/:id, when the shortenedURL is entered into browser, it redirects to the longURL
+// route handler for /u/:id, when the shortenedURL is entered into browser, it redirects user to the longURL
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
